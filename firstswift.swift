485e46a691452eef91cba9ed13009f2abca9a7a7
ramwert <ramranji@ramanarayanans-MacBook-Pro.local>
2014-12-21 00:49:11 -0500

    test

---
+++ firstswift.swift
@@ -0,0 +1,11 @@
+//
+//  firstswift.swift
+//  HelloWorld
+//
+//  Created by ramanarayanan balasubramanian on 12/20/14.
+//  Copyright (c) 2014 ramanarayanan balasubramanian. All rights reserved.
+//
+
+import Foundation
+let languageName: String = "Ram"
+
